e009072a9683495dced8c2e42c53d0c992aeeb6b
Dylan Davenport <ddavenport161@gmail.com>
2017-07-14 18:32:05 -0400

    Add files via upload

---
+++ squared.js
@@ -0,0 +1,8 @@
+// Define a function that takes a number as an argument 
+var square = function(num) {
+
+	// Returns that number times itself (square it)
+	return num * num;
+};
+// Log the results to the JavaScript console
+console.log(square(4)); 
